d072cb3e292371ad57e1fe7c7a1f245d58766e20
jdcanado <jdcanado@gmail.com>
2019-04-21 22:52:27 -0300

    Update index.py

--- index.py
+++ index.py
@@ -68,7 +68,7 @@ def get():
 @app.route('/api/v1/caminhoes', methods=['POST'])
 def post():
   if request.method == 'POST':
-    novo_caminhao = Caminhao(request.form['id'],request.form['tipo'])    
+    novo_caminhao = Caminhao(request.json['id'],request.json['tipo'])    
     db.session.add(novo_caminhao)
     db.session.commit()
     return caminhao_schema.jsonify(novo_caminhao)
